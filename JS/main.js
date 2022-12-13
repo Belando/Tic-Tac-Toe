834fa646cc74be3260e9d2a56d5413df94bbdc83
Francisco Belando <114404742+Narcoleptiic@users.noreply.github.com>
2022-12-13 01:17:15 +0100

    Update 2
    - Implementación de nueva fuente
    - Añadido contador para partidas

--- JS/main.js
+++ JS/main.js
@@ -1,65 +1,85 @@
-const cuadro_btn = document.querySelectorAll(".cuadro");
+/*Referencias de la API*/
+const cuadroBoton = document.querySelectorAll(".cuadro");
 const info = document.getElementById("info");
-const juego_btn = document.getElementById("boton3")
-const s = document.getElementById("boton2")
+const juegoBoton = document.getElementById("boton3")
+var gana1 = document.querySelector("#x")
+var gana2 = document.querySelector("#o")
+var jugador1 = document.getElementById("boton1")
+var jugador2 = document.getElementById("boton2")
+
+
+/*Funcion de botón de volver a jugar*/
 var i = 1;
-const jBtn_e =  "pointer-events:initial;opacity:initial;",
-      jBtn_d =  "pointer-events:none;opacity:40%;";
+const jugarActivado =  "pointer-events:initial;opacity:initial;",
+      jugarDesactivado =  "pointer-events:none;opacity:80%;";
 let state = false;
 
-var Victoria = [ [0,1,2],[3,4,5],[6,7,8],
+/*Marcador*/
+var contX = 1;
+var contO = 1;
+
+/*Posibles posiciones de victoria*/
+var victoria = [ [0,1,2],[3,4,5],[6,7,8],
              [0,3,6],[1,4,7],[2,5,8],
              [0,4,8],[2,4,6]
             ];
 
-function comprobar(){
-  juego_btn.style.cssText = jBtn_d;
-  for (var i = 0; i < Victoria.length;i++){
-    if(cuadro_btn[Victoria[i][0]].innerHTML === "X" && cuadro_btn[Victoria[i][1]].innerHTML === "X" && cuadro_btn[Victoria[i][2]].innerHTML === "X" ){
-      info.innerHTML = 'Jugador 1 Gana';
+/*Condición de victoria de cada jugador*/
+function quiengana(){
+  for (var i = 0; i < victoria.length;i++){
+    if(cuadroBoton[victoria[i][0]].innerHTML === "X" && cuadroBoton[victoria[i][1]].innerHTML === "X" && cuadroBoton[victoria[i][2]].innerHTML === "X" ){
+      info.innerHTML = "Jugador 1 Gana cositas";
       state = true;
-      deshabilitarCasillas();
-    }else if(cuadro_btn[Victoria[i][0]].innerHTML === "O" && cuadro_btn[Victoria[i][1]].innerHTML === "O" && cuadro_btn[Victoria[i][2]].innerHTML === "O"){
-      info.innerHTML = 'Jugador 2 Gana';
+      gana1.innerHTML = contX ++;
+      bloquearCasillas();
+    }else if(cuadroBoton[victoria[i][0]].innerHTML === "O" && cuadroBoton[victoria[i][1]].innerHTML === "O" && cuadroBoton[victoria[i][2]].innerHTML === "O"){
+      info.innerHTML = jugador2 + "Jugador 2 Gana cositas";
       state = true;
-      deshabilitarCasillas();
+      gana2.innerHTML = contO ++;
+      bloquearCasillas();
     }
   } 
 }
 
-function  deshabilitarCasillas(y){
+/*Ver quien empieza la partida(se turnan)*/
+function quienEmpieza(){
+  juegoBoton.style.cssText = jugarDesactivado;
+  let empezar;
+  (i % 2 == 0 )?empezar= "Jugador 1":empezar= "Jugador 2"; 
+  info.innerHTML = `Quien gane se lleva cositas: empieza ${empezar}.`;   
+}
+
+/*Bloqueo de casillas al ganar*/
+function  bloquearCasillas(y){
   (y == false)?i = Math.floor(Math.random() * (3 - 1)) + 1:0;
-    for(var n_boton = 0; n_boton < cuadro_btn.length; n_boton++){    
-      cuadro_btn[n_boton].style.setProperty("pointer-events","none"); 
+    for(var pos = 0; pos < cuadroBoton.length; pos++){    
+      cuadroBoton[pos].style.setProperty("pointer-events","none"); 
     } 
-  juego_btn.style.cssText = jBtn_e;
+  juegoBoton.style.cssText = jugarActivado;
 }
 
-function nEmpieza(){
-  juego_btn.style.cssText = jBtn_d;
-  let c1;
-  (i % 2 == 0 )?c1= "Jugador 1":c1= "Jugador 2"; 
-  info.innerHTML = `Quien gane se lleva cositas: empieza ${c1}.`;   
-}
-
-cuadro_btn.forEach(boton => {
+/* Turnos de juego */
+cuadroBoton.forEach(boton => {
   boton.onclick = function(){
     info.innerHTML = "";
     (i % 2 == 0)?boton.innerHTML = "X": boton.innerHTML = "O";
-    comprobar();
+    quiengana();
     boton.style.setProperty("pointer-events","none"); 
     i++; 
     (i == 3)?i = 1: 0 ;
   }
 });
 
-juego_btn.onclick = function(){
-  for(var n_boton = 0; n_boton < cuadro_btn.length; n_boton++){    
-    cuadro_btn[n_boton].style.cssText = "pointer-events:initial;";
-    cuadro_btn[n_boton].innerHTML = "";
+/*Reinicio del juego*/
+juegoBoton.onclick = function(){
+  for(var pos = 0; pos < cuadroBoton.length; pos++){    
+    cuadroBoton[pos].style.cssText = "pointer-events:initial;";
+    cuadroBoton[pos].innerHTML = "";
     state = false;
   }
-  nEmpieza();
+  quienEmpieza();
 }
 
-nEmpieza();
+/*Inicio del juego*/
+quienEmpieza();
+
